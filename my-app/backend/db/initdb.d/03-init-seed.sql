43c11ba80cec980dfa36a7a38c4315669b749273
Claudio Rea <crea@e4r7p1.42roma.it>
2024-10-24 20:47:29 +0200

    working on db initialization

--- my-app/backend/db/initdb.d/03-init-seed.sql
+++ my-app/backend/db/initdb.d/03-init-seed.sql
@@ -1,3 +1,4 @@
 USE myapp_db;
 
-INSERT INTO roles (role_name) VALUES ('admin'), ('user'), ('guest');
+INSERT INTO roles (role_name) VALUES ('admin'), ('user'), ('guest')
+ON DUPLICATE KEY UPDATE role_name=VALUES(role_name);
