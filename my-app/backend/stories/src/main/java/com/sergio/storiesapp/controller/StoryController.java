ecdca2389495c8137e33e7d64cbe1c902b5e3971
crea <crea@student.42roma.it>
2024-10-28 17:59:18 +0100

    working on stories container

--- my-app/backend/stories/src/main/java/com/sergio/storiesapp/controller/StoryController.java
+++ my-app/backend/stories/src/main/java/com/sergio/storiesapp/controller/StoryController.java
@@ -1,13 +1,22 @@
 package com.sergio.storiesapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @RestController
+@RequestMapping("/stories")
 public class StoryController {
 
-    @GetMapping("/stories/exempleroute")
-    public String sayHello() {
-        return "Hello, World!";
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @GetMapping
+    public String checkDatabase() {
+        String sql = "SELECT VERSION() as version";
+        String version = jdbcTemplate.queryForObject(sql, String.class);
+        return "Hello from MySQL " + version;
     }
 }
