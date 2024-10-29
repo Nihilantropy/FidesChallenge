b3335d84168d33b18d0b177a6e999947f7bcb39a
crea <crea@student.42roma.it>
2024-10-29 16:15:09 +0100

    restating claudio's branch with merge origin/merged

---
+++ my-app/backend/users/src/user_func/login.js
@@ -0,0 +1,66 @@
+import { V4 } from 'paseto'; // Using V4 version of Paseto for token creation
+import { Buffer } from 'buffer';
+import db from '../database.js'; // Assuming you have a configured knex instance
+import bcrypt from 'bcrypt'; // Assuming you're storing hashed passwords in the DB
+import { InvalidCredentialsError, TokenCreationError, NoCredentialError, InternalServerError } from '../err/dist/CustomError.js';
+
+const privateKey = Buffer.from(process.env.PRIVATE_KEY.replace(/\\n/g, '\n'), 'utf-8');
+
+async function loginUser(email, password) {
+
+    if (!email || !password) {
+        throw new NoCredentialError(); // Throw NoCredentialError if either is missing
+    }
+
+	console.log("Attempting to login user:", email);  // Debug log
+
+	let user;
+
+	// Handle fetch query error
+	try {
+		user = await db('users').where({ email }).first();
+	}
+	catch (e) {
+		console.error("user not found");
+	}
+
+	// If the user is not found
+	if (!user) {
+		throw new InvalidCredentialsError(); // Use the custom error
+	}
+	
+	console.log("User found:", user);  // Debug log
+	if (password !== user.password) {
+		throw new InvalidCredentialsError(); // TODO remove this, uncomment below
+	}
+
+	// Check if the provided password matches the hashed password in the DB
+	// const passwordMatch = await bcrypt.compare(password, user.password);
+	// if (!passwordMatch) {
+	// 	throw new InvalidCredentialsError(); // Use the custom error
+	// }
+
+	// If password matches, generate a Paseto token
+	try {
+		const token = await V4.sign(
+			{ id: user.id, email: user.email }, // Payload
+			privateKey, // Secret key
+			{ expiresIn: '1h' } // Token expiration time
+		);
+		
+	
+		if (!token) {
+			throw new TokenCreationError();
+		}
+
+		console.log("token created!");
+		return token;
+	
+	} catch (error) {
+		console.error("Error during token generation:", error);
+		throw new InternalServerError("Token generation failed");
+	}
+}
+
+// Export the loginUser function
+export default loginUser;
---
+++ my-app/frontend/sergio/pages/Story/NewStory.js
@@ -0,0 +1,92 @@
+import React, {  useState, useEffect } from 'react';
+import { TextInput, View, Text, Pressable, ScrollView, Platform } from 'react-native';
+import styles from './../../assets/style/main.js';
+
+const Content_web = ({ showPage, invia, setStory, getStory }) =>{
+  return (
+    <ScrollView>
+      <View style={styles.box}>
+        <View style={styles.footerapp2}>
+          <Pressable onPress={() => showPage(2)}><Text style={styles.link}>Annulla</Text></Pressable>
+          <Text style={styles.testi}> | </Text>
+          <Pressable onPress={invia}><Text style={styles.link}>Racconta</Text></Pressable>
+        </View>
+        <TextInput multiline={true} numberOfLines={10} spellCheck={false} style={styles.textarea} placeholder="Scrivi qui la tua storia..." value={getStory()} onChangeText={(text) => setStory(text)} />
+      </View>
+    </ScrollView>
+  )
+};
+
+const Content_app = ({ getStory, setStory }) =>{
+  return (
+    <ScrollView>
+      <View style={styles.box}>
+        <TextInput multiline={true} numberOfLines={10} spellCheck={false} style={styles.textarea} placeholder="Scrivi qui la tua storia..." value={getStory()} onChangeText={(text) => setStory(text)} />
+      </View>
+    </ScrollView>
+  )
+};
+
+const validator = require('validator');
+const NewStory = ({ showPage, gWVTtoken, sShowPopupFB, setInviaFunction, setShowErr }) => {
+  // useEffect(() => {
+  //   if (gWVTtoken() == ''){
+  //     showPage(2);
+  //     sShowPopupFB(true);
+  //   }
+  // }, [gWVTtoken, showPage, sShowPopupFB]);
+
+  useEffect(() => {
+    setInviaFunction(invia);
+  }, [invia]);
+
+  const [story, setStory] = useState('');
+  const getStory = (page) => { return story; };
+  function invia() {
+    /* ====== Basic Check ====== */
+    if (story === '') {
+        setShowErr("Completa tutti i campi prima di proseguire");
+        return;
+    }
+
+    /* ====== Sanitized ====== */
+    const sanitizedStory = validator.escape(story);
+    
+    /* ====== Send post ====== */
+    console.log("invio fetch = Story:"+sanitizedStory);
+    try {
+        fetch("http://localhost:3000/new_story", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                storia: sanitizedStory,
+                jwt: gWVTtoken()
+            }),
+        })
+        .then(result => {
+            if (!result) return;
+            const { status, body } = result;
+            if (status != 200) {
+                setShowErr(body.message);
+            }else{
+              showPage(2);
+            }
+        })
+        .catch(error => {
+          setShowErr("Errore interno");
+        });
+    }catch (error) {
+        setShowErr("Errore interno");
+        return ;
+    }
+  }
+  return (
+    <View style={styles.stacca}>
+      { Platform.OS === 'web' && <Content_web showPage={showPage} invia={invia} setStory={setStory} getStory={getStory} /> }
+      { Platform.OS !== 'web' && <Content_app getStory={getStory} setStory={setStory} /> }
+    </View>
+  );
+};
+export default NewStory;
