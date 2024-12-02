16d151e50a430934b3a864735a934e72407823b7
Nihilantropy <crea@student.42roma.it>
2024-12-02 22:05:57 +0100

    pushing latest changes

--- my-app/backend/db/initdb.d/6_create_mysql_user.sql
+++ my-app/backend/db/initdb.d/6_create_mysql_user.sql
@@ -3,7 +3,7 @@ CREATE USER 'mysql_user'@'%' IDENTIFIED BY 'userpassword';
 -- ALTER USER 'mysql_user'@'%' IDENTIFIED WITH mysql_native_password BY 'userpassword';
 
 -- Grant privileges on all tables except `admins`
-GRANT CREATE SELECT, INSERT, UPDATE, DELETE ON myapp_db.* TO 'mysql_user'@'%';
+GRANT SELECT, INSERT, UPDATE, DELETE ON myapp_db.* TO 'mysql_user'@'%';
 REVOKE ALL PRIVILEGES ON myapp_db.admins FROM 'mysql_user'@'%';
 
 -- Apply privilege changes
